44b1d35e6b7b3db964aa16c004e3403ada963ec2
Cole Reveal <alexi.reveal@gmail.com>
2014-05-16 12:28:28 -0500

    added functions to our todo list for better readability

--- todo.php
+++ todo.php
@@ -1,38 +1,65 @@
 <?php
 
 // Create array to hold list of todo items
+// Create array to hold list of todo items
 $items = array();
+
+// List array items formatted for CLI
+function list_items($array)
+{
+    $list = '';
+    
+    foreach($array as $key => $value) {
+        $key++;
+        // Display each item and a newline
+        $list .= "[{$key}] {$value}\n";
+    }
+    
+    return $list;
+
+}
+
+
+
+
+// Get STDIN, strip whitespace and newlines, 
+// and convert to uppercase if $upper is true
+
+function get_input($upper = FALSE) 
+{
+    if($upper) {
+        return strtoupper(trim(fgets(STDIN)));
+    } else {
+        return trim(fgets(STDIN));
+    }
+}
+
 
 // The loop!
 do {
-    // Iterate through list items
-    foreach ($items as $key => $item) {
-        // Display each item and a newline
-        $fix = $key + 1;
-        echo "[{$fix}] {$item}\n";
-    }
 
+    echo list_items($items);
     // Show the menu options
     echo '(N)ew item, (R)emove item, (Q)uit : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
-    $input = strtoupper(trim(fgets(STDIN)));
+    $input = strtoupper(get_input(true));
 
     // Check for actionable input
     if ($input == 'N') {
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
-        $items[] = trim(fgets(STDIN));
+        $items[] = get_input();
     } elseif ($input == 'R') {
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
-        $key = trim(fgets(STDIN));
+        $key = get_input();
         // Remove from array
         unset($items[$key-1]);
-
+        //reset index counter
         $items = array_values($items);
     }
 // Exit when input is (Q)uit
@@ -43,5 +70,6 @@ echo "Goodbye!\n";
 
 // Exit with 0 errors
 exit(0);
+
 
 ?>
